32e6522db497e6e8bb32556addcffb0ef769aabe
mikhelbriones <mike.briones@smsupermalls.com>
2022-09-19 01:34:16 +0800

    Ep21: Implement Login API with PASETO and JWT

--- api/account_test.go
+++ api/account_test.go
@@ -80,7 +80,7 @@ func TestGetAccountAPI(t *testing.T) {
 			tc.buildStubs(store)
 
 			// start test server amd semd request
-			server := NewServer(store)
+			server := newTestServer(t, store)
 			recorder := httptest.NewRecorder()
 
 			url := fmt.Sprintf("/accounts/%d", tc.accountID)
--- api/server.go
+++ api/server.go
@@ -1,20 +1,48 @@
 package api
 
 import (
+	"fmt"
+
 	"github.com/gin-gonic/gin"
 	"github.com/gin-gonic/gin/binding"
 	"github.com/go-playground/validator/v10"
 	db "github.com/mikhelbriones/simplebank/db/sqlc"
+	"github.com/mikhelbriones/simplebank/token"
+	"github.com/mikhelbriones/simplebank/util"
 )
 
 // Server serves HTTP request for our banking service.
 type Server struct {
-	store  db.Store
-	router *gin.Engine
+	config     util.Config
+	store      db.Store
+	tokenMaker token.Maker
+	router     *gin.Engine
 }
 
-func NewServer(store db.Store) *Server {
-	server := &Server{store: store}
+func NewServer(config util.Config, store db.Store) (*Server, error) {
+	tokenMaker, err := token.NewPasetoMaker(config.TokenSymmetricKey)
+	if err != nil {
+		return nil, fmt.Errorf("cannot create token maker: %v", err)
+	}
+
+	server := &Server{
+		config:     config,
+		store:      store,
+		tokenMaker: tokenMaker,
+	}
+	server.setupRouter()
+	return server, nil
+}
+
+func errorResponse(err error) gin.H {
+	return gin.H{"error": err.Error()}
+}
+
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
+}
+
+func (server *Server) setupRouter() {
 	router := gin.Default()
 
 	if v, ok := binding.Validator.Engine().(*validator.Validate); ok {
@@ -25,14 +53,7 @@ func NewServer(store db.Store) *Server {
 	router.GET("/accounts", server.listAccounts)
 	router.POST("/transfers", server.createTransfer)
 	router.POST("/users", server.createUser)
+	router.POST("/users/login", server.loginUser)
+
 	server.router = router
-	return server
-}
-
-func errorResponse(err error) gin.H {
-	return gin.H{"error": err.Error()}
-}
-
-func (server *Server) Start(address string) error {
-	return server.router.Run(address)
 }
